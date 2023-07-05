961d883d07bcd6ddf8fa56668ddad8f413f5750c
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2023-07-06 01:30:05 +0300

    start button and desctiption of starbucks barista is added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  'Bir fincan uzağınızda',
+                  'Just a cup away',
                   style: GoogleFonts.overlock(
                     fontSize: 20,
                     color: Colors.white,
@@ -45,39 +46,79 @@ class MyApp extends StatelessWidget {
                     color: Colors.black,
                   ),
                 ),
-                Card(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 45.0,
-                  ),
+                Container(
+                  padding: EdgeInsets.all(25.0),
+                  margin: EdgeInsets.symmetric(horizontal: 40.0),
                   color: Colors.blueGrey[900],
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
+                  child: Text(
+                    'Welcome to Starbucks Recipe! In our application, '
+                    'you will discover how each coffee fascinates you. '
+                    'We offer a unique experience by sharing with you the coffee'
+                    ' ingredients and how they are prepared.',
+                    style: GoogleFonts.overlock(
+                      fontSize: 16,
                       color: Colors.white,
-                    ),
-                    title: Text(
-                      'siparis@starbuckscoffe.com',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                      ),
                     ),
                   ),
                 ),
+                // Card(
+                //   margin: EdgeInsets.symmetric(
+                //     horizontal: 45.0,
+                //   ),
+                //   color: Colors.blueGrey[900],
+                //   child: ListTile(
+                //     leading: Icon(
+                //       Icons.email,
+                //       color: Colors.white,
+                //     ),
+                //     title: Text(
+                //       'siparis@starbuckscoffe.com',
+                //       style: TextStyle(
+                //         color: Colors.white,
+                //         fontSize: 15.0,
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // SizedBox(
+                //   height: 10.0,
+                // ),
+                // Card(
+                //   margin: EdgeInsets.symmetric(horizontal: 45.0),
+                //   color: Colors.blueGrey[900],
+                //   child: ListTile(
+                //     leading: Icon(
+                //       Icons.phone,
+                //       color: Colors.white,
+                //     ),
+                //     title: Text(
+                //       '+90 555 555 55 55',
+                //       style: TextStyle(color: Colors.white, fontSize: 15.0),
+                //     ),
+                //   ),
+                // ),
                 SizedBox(
                   height: 10.0,
                 ),
-                Card(
-                  margin: EdgeInsets.symmetric(horizontal: 45.0),
-                  color: Colors.blueGrey[900],
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      '+90 555 555 55 55',
-                      style: TextStyle(color: Colors.white, fontSize: 15.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 45.0, vertical: 40.0),
+                  child: TextButton(
+                    style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.brown[500]!)),
+                    onPressed: () {},
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.coffee_rounded,
+                        color: Colors.brown[900],
+                      ),
+                      title: Text(
+                        'Let\'s find out the recipe!',
+                        style: TextStyle(color: Colors.white, fontSize: 15.0),
+                      ),
                     ),
                   ),
                 ),
