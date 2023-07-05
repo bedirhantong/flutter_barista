3935dbc49ad5e794960db1d2f651dd4282496fdd
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2023-07-06 00:16:31 +0300

    Responsive UI is detailed with Card,ListTile,Column and Row widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,9 +26,59 @@ class MyApp extends StatelessWidget {
                 Text(
                   'Starbucks',
                   style: TextStyle(
-                    fontFamily: 'Satisfy-Regular.ttf',
+                    fontFamily: 'Satisfy',
                     fontSize: 45,
-                    color: Colors.brown[900],
+                    color: Colors.brown[800],
+                  ),
+                ),
+                Text(
+                  'Bir fincan uzağınızda',
+                  style: GoogleFonts.overlock(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: Divider(
+                    height: 30.0,
+                    color: Colors.black,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 45.0,
+                  ),
+                  color: Colors.blueGrey[900],
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'siparis@starbuckscoffe.com',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 45.0),
+                  color: Colors.blueGrey[900],
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      '+90 555 555 55 55',
+                      style: TextStyle(color: Colors.white, fontSize: 15.0),
+                    ),
                   ),
                 ),
               ],
