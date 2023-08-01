c19177314b21e2ae9045617fae2871002f70b80e
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2023-08-02 01:04:47 +0300

    issue solved : Navigator operation requested with a context that does not include a Navigator.

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barista/pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const Home());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF685BFF)),
+        useMaterial3: true,
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  radius: 70.0,
-                  backgroundImage: AssetImage('assets/images/starbucks.png'),
+      home: const MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 70.0,
+                backgroundImage: AssetImage('assets/images/starbucks.png'),
+              ),
+              Text(
+                'Starbucks',
+                style: TextStyle(
+                  fontFamily: 'Satisfy',
+                  fontSize: 45,
+                  color: Colors.brown[800],
                 ),
-                Text(
-                  'Starbucks',
-                  style: TextStyle(
-                    fontFamily: 'Satisfy',
-                    fontSize: 45,
-                    color: Colors.brown[800],
-                  ),
+              ),
+              Text(
+                'Just a cup away',
+                style: GoogleFonts.overlock(
+                  fontSize: 20,
+                  color: Colors.white,
                 ),
-                Text(
-                  'Just a cup away',
+              ),
+              const SizedBox(
+                width: 200,
+                child: Divider(
+                  height: 30.0,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(25.0),
+                margin: const EdgeInsets.symmetric(horizontal: 40.0),
+                color: Colors.blueGrey[900],
+                child: Text(
+                  'Welcome to Starbucks Recipe! In our application, '
+                  'you will discover how each coffee fascinates you. '
+                  'We offer a unique experience by sharing with you the coffee'
+                  ' ingredients and how they are prepared.',
                   style: GoogleFonts.overlock(
-                    fontSize: 20,
+                    fontSize: 16,
                     color: Colors.white,
                   ),
                 ),
-                Container(
-                  width: 200,
-                  child: Divider(
-                    height: 30.0,
-                    color: Colors.black,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(25.0),
-                  margin: EdgeInsets.symmetric(horizontal: 40.0),
-                  color: Colors.blueGrey[900],
-                  child: Text(
-                    'Welcome to Starbucks Recipe! In our application, '
-                    'you will discover how each coffee fascinates you. '
-                    'We offer a unique experience by sharing with you the coffee'
-                    ' ingredients and how they are prepared.',
-                    style: GoogleFonts.overlock(
-                      fontSize: 16,
-                      color: Colors.white,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.brown[500]!)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
                     ),
-                  ),
+                  );
+                },
+                child: const Text(
+                  'Let\'s find out the recipe!',
+                  style: TextStyle(color: Colors.white, fontSize: 15.0),
                 ),
-                // Card(
-                //   margin: EdgeInsets.symmetric(
-                //     horizontal: 45.0,
-                //   ),
-                //   color: Colors.blueGrey[900],
-                //   child: ListTile(
-                //     leading: Icon(
-                //       Icons.email,
-                //       color: Colors.white,
-                //     ),
-                //     title: Text(
-                //       'siparis@starbuckscoffe.com',
-                //       style: TextStyle(
-                //         color: Colors.white,
-                //         fontSize: 15.0,
-                //       ),
-                //     ),
-                //   ),
-                // ),
-                // SizedBox(
-                //   height: 10.0,
-                // ),
-                // Card(
-                //   margin: EdgeInsets.symmetric(horizontal: 45.0),
-                //   color: Colors.blueGrey[900],
-                //   child: ListTile(
-                //     leading: Icon(
-                //       Icons.phone,
-                //       color: Colors.white,
-                //     ),
-                //     title: Text(
-                //       '+90 555 555 55 55',
-                //       style: TextStyle(color: Colors.white, fontSize: 15.0),
-                //     ),
-                //   ),
-                // ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 45.0, vertical: 40.0),
-                  child: TextButton(
-                    style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.brown[500]!)),
-                    onPressed: () {},
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.coffee_rounded,
-                        color: Colors.brown[900],
-                      ),
-                      title: Text(
-                        'Let\'s find out the recipe!',
-                        style: TextStyle(color: Colors.white, fontSize: 15.0),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
